0c2b6e78bfd3064ec84a4f98e701cce11d0813c7
ZenZiggy <cavecreeper7@gmail.com>
2026-09-16 21:07:34 -0400

    Full Code Update
    Implemented logic to find faults in the resistor line.
    
    Removed LCD panel due to unsolvable ECU errors.

--- sketch_arduino-fault-finder/sketch_arduino-fault-finder.ino
+++ sketch_arduino-fault-finder/sketch_arduino-fault-finder.ino
@@ -1,26 +1,50 @@
-#include <LiquidCrystal.h>
-
-//LCD pin declarations.
-const int rs = 2;
-const int en = 3;
-const int d4 = 6;
-const int d5 = 7;
-const int d6 = 8;
-const int d7 = 9;
-
-//LCD declared.
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const int analogPin = A0;
 
 void setup() 
 {
-
-//Initialize LCD.
-Serial.begin(9600);
-lcd.begin(16, 2);
-
+  Serial.begin(9600);
+  Serial.println("Transmission Line Fault Finder Initialized.");
 }
 
 void loop() 
 {
+  int adcValue = analogRead(analogPin);
 
+
+  float voltage = adcValue * (5.0 / 1023);
+  
+  Serial.println("ADC: ");
+  Serial.print(adcValue);
+  Serial.print(" | Voltage: ");
+  Serial.print(voltage);
+  Serial.print("V | Status: ");
+
+
+  if (adcValue > 950)
+  {
+    Serial.println("Grid Normal (No Faults Detected).");
+  }
+  else if (adcValue < 100) 
+  {
+    Serial.println("CRITICAL: Fault at Zone 1 (Substation | Distance: 0km).");
+  }
+  else if (adcValue >= 450 && adcValue <= 550) 
+  {
+    Serial.println("Fault detected at Zone 2 (Distance: 10km).");
+  }
+  else if (adcValue >= 630 && adcValue <= 720)
+  {
+    Serial.println("Fault detected at Zone 3 (Distance 20km).");
+  }
+  else if (adcValue >= 730 && adcValue <= 790)
+  {
+    Serial.println("Fault detected at Zone 4 (Distance 30km).");
+  }
+  else if (adcValue >= 791 && adcValue <= 860)
+  {
+    Serial.println("Fault detected at Zone 5 (Distance 40km).");
+  }
+
+
+  delay(1000);
 }
